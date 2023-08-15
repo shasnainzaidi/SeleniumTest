c3dca851872aa1b6900e0776cc9bfba7c110643d
amir <haq8948@gmail.com>
2023-08-15 12:21:12 +0500

    selenium basics

---
+++ src/test/java/Basics.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Basics {
+
+
+    private WebDriver driver;
+
+    @Test
+    public void SeleniumBasics() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+//        TestCase
+//         Visit URL
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+//         Enter name
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Hello");
+//         click Confirm
+        driver.findElement(By.cssSelector("input[id='confirmbtn']")).click();
+
+//         Confirm Alert
+
+        driver.switchTo().alert().accept();
+
+        driver.quit();
+
+    }
+
+
+}
