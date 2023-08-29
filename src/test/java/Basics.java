409d8738ec0e8d8e94bc088ee1ffe15be007b36b
hasnain.zaidi <hasnain.xaidi@outlook.com>
2023-08-29 11:36:49 +0500

    Radio button case

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,7 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -27,10 +32,15 @@ public class Basics {
 //         Confirm Alert
 
         driver.switchTo().alert().accept();
-
+     // Radio Buttons
+    	List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[class='radioButton']"));
+    	for(int i = 0; i <= 2; i++) {
+    	    WebElement getOne = radioButtons.get(i);
+    	    getOne.click();
+    	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    	}
+    	
         driver.quit();
 
     }
-
-
-}
+ }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -29,6 +29,7 @@ public void BasicTest() {
 	driver.switchTo().alert().accept();
 	
 	
+	}
 	
 	
 	
